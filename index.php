82969124d01850d2eaf37c58d1adb7feab91b2e2
Baibhav Chakraborty <baibhav@ialoy.com>
2022-11-18 12:52:24 +0530

    Added meta description tag
    Signed-off-by: Baibhav Chakraborty <baibhav@ialoy.com>

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en" ng-app="cms">
     <head>
         <meta charset="UTF-8">
+        <meta name="description" content="This is a construction management system">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="resources/css/bootstrap.min.css" type="text/css">
